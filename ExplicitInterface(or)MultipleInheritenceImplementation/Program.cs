9d22fc171ee387a8354973eb4313c201405a569a
Dell <Dell@DESKTOP-13B42NJ>
2026-02-15 09:07:00 +0530

    Add project files.

---
+++ ExplicitInterface(or)MultipleInheritenceImplementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface_or_MultipleInheritenceImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object obj1 = new object();//object is base class for all the classes.
+
+
+            // Test obj=new Test();//you can't use normal object for calling the explicit methods.
+            //obj.//you will not get any intellesense here
+            //You can call the explicit interfce methods below way.
+            Test obj = new Test();
+
+            IFirst firstObj = obj;
+            firstObj.Show();//calling the IFirst interface Show() Method.
+
+            ISecond secondObj = obj;
+            secondObj.Show();//calling the ISecond interface Show() Method.
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ExplicitInterface(or)MultipleInheritenceImplementation/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface_or_MultipleInheritenceImplementation
+{
+    //In c#.net Multiple Inheritence is not possible by using classes.
+    //Through interface it is possible.Beacuse if 2 interfaces having same method name compiler doesn't know which method i need to call.
+    //due to that through explict interface concept we can achoeve multiple inhertence.
+    internal class Test : IFirst, ISecond
+    {
+        void IFirst.Show()
+        {
+            Console.WriteLine("IFirst Show");
+        }
+        void ISecond.Show()
+        {
+            Console.WriteLine("ISecond Show");
+        }
+    }
+}
